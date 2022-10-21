54c175a0bb59fe7c35b1d89146510049c49f2d8d
tjugosex <mikael.skaremyr.te1@hotmail.com>
2022-10-21 20:11:34 +0000

    pages up

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,17 @@
 import "../styles/globals.css";
-import Head from 'next/head';
-
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div style={{backgroundColor: 'rgb(20, 20, 20)'}}>
       <Head>
         <title>Mikel</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
-
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  TagRightIcon,
+  TagCloseButton,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from "@chakra-ui/react";
 
 export default function Home() {
   if (process.browser) {
@@ -16,19 +40,38 @@ export default function Home() {
       }
     );
   }
-  
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div id="menu">
       <div id="menu-items">
         <div class="menu-item">Hem</div>
-        <a href="https://github.com/tjugosex/" class="menu-item" >Github</a>
+        <a href="https://github.com/tjugosex/" class="menu-item">
+          Github
+        </a>
         <div class="menu-item">Linkedin</div>
-        <div class="menu-item">Kontakt</div>
+        <div class="menu-item" onClick={onOpen}>
+          Kontakt
+        </div>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Kontaktuppgifter</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <UnorderedList spacing={3}>
+              <ListItem><Tag colorScheme="purple">Mail: hejhej@yeet.com</Tag></ListItem>
+              <ListItem><Tag colorScheme="purple">Telefon: 99999999</Tag></ListItem>
+              </UnorderedList>
+            </ModalBody>
+
+            <ModalFooter></ModalFooter>
+          </ModalContent>
+        </Modal>
       </div>
       <div id="menu-background-pattern"></div>
       <div id="menu-background-image"></div>
     </div>
   );
 }
-
